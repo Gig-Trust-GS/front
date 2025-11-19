944f9cbe22f72847b976f1bc656e1816660d24ad
MatheusSansao <Matheussansao@hotmail.com>
2025-11-19 13:11:30 -0300

    feat(config): Adiciona paleta de cores base para Dark Mode no tailwind.config.js

---
+++ gig/src/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        exttend: {
+            colors: {
+                // Paleta de cores personalizadas para o tema escuro (Dark Mode)
+                'dark-bg': '#0C0C0C', // Cor primaria de fundo da aplicação.
+                'dark-card': '#2A2A2A' //Cor de fundo utilizada para cards, modais e elementos em destaque.
+            },
+        },
+    },
+}
